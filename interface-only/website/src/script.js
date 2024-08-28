f348025b9fbd50d6610d273e04cfa47ea0ede08c
GTPSHAX <rafiehasannudin@gmail.com>
2024-08-27 22:50:38 -0700

    interface only (landing page/login page)

---
+++ interface-only/website/src/script.js
@@ -0,0 +1,12 @@
+// document.getElementById('togglePassword').addEventListener('click', function () {
+//     const passwordInput = document.getElementById('passwordInput');
+//     const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+//     passwordInput.setAttribute('type', type);
+
+//     const svg = this.querySelector('svg');
+//     if (type === 'password') {
+//         svg.setAttribute('fill', 'currentColor');
+//     } else {
+//         svg.setAttribute('fill', '#1e3a8a');
+//     }
+// });
---
+++ interface-only/website/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ['./src/*.{js,ts,jsx,tsx,html}'],
+    theme: {
+      extend: {
+        animation: {
+          'pulse-slow': 'pulse 10s linear infinite',
+        }
+      }
+    }
+  }
